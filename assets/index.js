f2e8a711d7f84c18d7170c07832b43e602ebd25e
Nischal Bhattarai <nishchalb21@gmail.com>
2024-04-08 15:03:32 +0545

    added search functionality

--- assets/index.js
+++ assets/index.js
@@ -1,90 +1,103 @@
+// Inserting the top header 
+const headerTop = document.querySelector("#header-top");
+headerTop.innerHTML = `
+    <div class="social-icons">
+        <a href="https://www.facebook.com/thenischalbhattarai/" target="_blank"><i class="ri-facebook-fill"></i></a>
+        <a href=""><i class="ri-instagram-line" target="_blank"></i></a>
+        <a href="https://twitter.com/nischal_108" target="_blank"><i class="ri-twitter-x-line"></i></a>
+        <a href="https://github.com/nischal108" target="_blank"><i class="ri-github-fill"></i></a>
+    </div>
+    <h5 id="header-info">FREE SHIPPING THIS WEEK ORDER OVER - Rs.550</h5>
+    <h6 id="seller-call">become a seller</h6>`;
 
-let productsContainer = document.querySelector("#products-cards-list");
+// Inserting header bottom 
+const headerBottom = document.getElementById("header-bottom");
+headerBottom.innerHTML = `
+    <a href="/../index.html"><h3>Shopiji</h3></a>
+    <div id="search">
+        <input type="text" placeholder="search items ....">
+        <i class="ri-search-2-line"></i>
+    </div>
+    <div id="menu-icons">
+        <i class="ri-search-2-line"></i>
+        <a href="assets/favourites-page/favourite.html"><i class="ri-heart-2-line"></i></a>
+        <a href=""><i class="ri-shopping-cart-fill"></i></a>
+    </div>`;
 
+
+
+// product card creation
+const productsContainer = document.querySelector("#products-cards-list");
 
 function loadProducts() {
     products.forEach(product => {
-        // Create product card container
-        let productCard = document.createElement("div");
+        const productCard = document.createElement("div");
         productCard.classList.add("product-card");
 
-        // Create product image container
-        let productImage = document.createElement("div");
+        const productImage = document.createElement("div");
         productImage.classList.add("product-image");
 
-        // Create main product image
-        let mainImage = document.createElement("img");
+        const mainImage = document.createElement("img");
         mainImage.src = product.image1;
         mainImage.alt = "Product Image 1";
 
-        // Create hover image
-        let hoverImage = document.createElement("img");
+        const hoverImage = document.createElement("img");
         hoverImage.src = product.image2;
         hoverImage.alt = "Product Image 2";
         hoverImage.classList.add("hover-image");
 
-        // Create menu container
-        let menu = document.createElement("div");
+        const menu = document.createElement("div");
         menu.classList.add("menu");
 
-        // Create menu items
-        let ul = document.createElement("ul");
-        let likeButton = document.createElement("button");
+        const ul = document.createElement("ul");
+        const likeButton = document.createElement("button");
         likeButton.classList.add("like-button");
         likeButton.innerHTML = '<i class="ri-heart-2-fill"></i>';
-        let moreButton = document.createElement("button");
+        const moreButton = document.createElement("button");
         moreButton.classList.add("more-button");
         moreButton.innerHTML = '<i class="ri-more-line"></i>';
         ul.appendChild(document.createElement("li")).appendChild(likeButton);
         ul.appendChild(document.createElement("li")).appendChild(moreButton);
         menu.appendChild(ul);
 
-        // Append image elements to product image container
         productImage.appendChild(mainImage);
         productImage.appendChild(hoverImage);
         productImage.appendChild(menu);
 
-        // Create product info container
-        let productInfo = document.createElement("div");
+        const productInfo = document.createElement("div");
         productInfo.classList.add("product-info");
 
-        // Create product name
-        let productName = document.createElement("h3");
+        const productName = document.createElement("h3");
         productName.classList.add("product-name");
         productName.textContent = product.name;
 
-        // Create product description
-        let productDescription = document.createElement("p");
+        const productDescription = document.createElement("p");
         productDescription.classList.add("product-description");
         productDescription.textContent = product.description;
 
-        // Create brand
-        let brand = document.createElement("div");
+        const brand = document.createElement("div");
         brand.classList.add("brand");
         brand.textContent = "Brand: " + product.brand;
 
-        // Create product marked price
-        let markedPrice = document.createElement("div");
+        const markedPrice = document.createElement("div");
         markedPrice.classList.add("product-marked-price");
         markedPrice.textContent = "$" + product.original_price.toFixed(2);
 
-        // Create product price
-        let productPrice = document.createElement("div");
+        const productPrice = document.createElement("div");
         productPrice.classList.add("product-price");
-        productPrice.innerHTML = "Price: <span>$" + product.selling_price.toFixed(2) + "</span>";
+        productPrice.innerHTML = `
+            <button class="add-to-cart-button">Add to Cart</button>
+            Price: <span>$${product.selling_price.toFixed(2)}</span>`;
 
-        // Append product info elements to product info container
         productInfo.appendChild(productName);
         productInfo.appendChild(productDescription);
         productInfo.appendChild(brand);
         productInfo.appendChild(markedPrice);
         productInfo.appendChild(productPrice);
 
-        // Append product image and info containers to product card
         productCard.appendChild(productImage);
         productCard.appendChild(productInfo);
 
-        // Append product card to products container
         productsContainer.appendChild(productCard);
     });
 }
@@ -92,39 +105,76 @@ function loadProducts() {
 // Call loadProducts function to populate product cards
 loadProducts();
 
-
-
-// set timer for the product of the day
-
+// Set timer for the product of the day
 const daysSelected = document.getElementById("days");
 const hoursSelected = document.getElementById("hours");
 const minutesSelected = document.getElementById("minutes");
 const secondsSelected = document.getElementById("seconds");
 
-// Set the end date for the countdown
 const endDate = new Date();
-// Set the end date to 4 days ahead
-endDate.setDate(endDate.getDate() + 4); 
+endDate.setDate(endDate.getDate() + 4); // Set the end date to 4 days ahead
 
 function updateCountdown() {
     const now = new Date();
-
-
-   // the difference between these time stamps gives difference in miliseconds
     const timeDifference = endDate - now;
 
-        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-        daysSelected.textContent = days;
-        hoursSelected.textContent = hours;
-        minutesSelected.textContent = minutes;
-        secondsSelected.textContent = seconds;
+    daysSelected.textContent = days;
+    hoursSelected.textContent = hours;
+    minutesSelected.textContent = minutes;
+    secondsSelected.textContent = seconds;
 }
 
 // Call updateCountdown every second
 const timerInterval = setInterval(updateCountdown, 1000);
 
 updateCountdown();
+
+
+
+// For search functionality at the header bottom
+const search = document.querySelector("#search input");
+
+
+search.addEventListener("input", () => {
+    const searchTerm = search.value.toLowerCase().trim();
+    const productCards = document.querySelectorAll(".product-card");
+    const hero = document.querySelector("#hero-section");
+    const productOfDay = document.querySelector("#main-product-area");
+    const productofDayContain = document.querySelector("#products-area-container");
+    const noProduct = document.querySelector("#no-products-found");
+    let hasMatch = false;
+
+    if (searchTerm !== "") {
+        productofDayContain.style.display = "none";
+        productOfDay.style.display = "none";
+        hero.style.display = "none";
+    } else {
+        productofDayContain.style.display = "";
+        productOfDay.style.display = "";
+        hero.style.display = "";
+    }
+
+    productCards.forEach(card => {
+        const productName = card.querySelector(".product-name").textContent.toLowerCase();
+        const productDescription = card.querySelector(".product-description").textContent.toLowerCase();
+
+        if (productName.includes(searchTerm) || productDescription.includes(searchTerm)) {
+            card.style.display = ""; // Show the product card if it matches the search term
+            hasMatch = true;
+        } else {
+            card.style.display = "none"; // Hide the product card if it doesn't match the search term
+        }
+    });
+
+    // Display no results message if there are no matching products
+    if (!hasMatch) {
+        noProduct.style.display="block";
+    } else {
+        noProduct.style.display="none"
+    }
+});
